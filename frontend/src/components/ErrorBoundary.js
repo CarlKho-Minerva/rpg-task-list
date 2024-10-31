745dae39dd7cc2a0cef5635b1a745948308b727f
Carl Kho <106736711+CarlKho-Minerva@users.noreply.github.com>
2024-10-31 14:49:32 -0300

    refact: Clear FE BE diff. Clean slate.

--- frontend/src/components/ErrorBoundary.js
+++ frontend/src/components/ErrorBoundary.js
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { Component } from 'react';
 
-class ErrorBoundary extends React.Component {
+class ErrorBoundary extends Component {
   constructor(props) {
     super(props);
     this.state = { hasError: false };
@@ -11,12 +11,12 @@ class ErrorBoundary extends React.Component {
   }
 
   componentDidCatch(error, errorInfo) {
-    console.error("Uncaught error:", error, errorInfo);
+    console.error("ErrorBoundary caught an error", error, errorInfo);
   }
 
   render() {
     if (this.state.hasError) {
-      return <h1>Something went wrong. Please refresh the page.</h1>;
+      return <h1>Something went wrong.</h1>;
     }
 
     return this.props.children;
